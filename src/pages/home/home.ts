15818b95c3a99ae12877fc3d9268f1fb913f7e9c
schemr <schemryj@gmail.com>
2017-01-11 13:34:40 +0900

    http://blog.ionic.io/10-minutes-with-ionic-2-calling-an-api/?_ga=1.3303732.526993330.1481937814 진행

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { PeopleService } from '../../providers/people-service';
+
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [PeopleService]
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+export class HomePage {
+  public people: any;
+  
+  constructor(public navCtrl: NavController, public peopleService: PeopleService) {
+    this.loadPeople();
   }
 
+  loadPeople(){
+    this.peopleService.load()
+    .then(data=>{
+      this.people = data;
+    });
+  }
 }
